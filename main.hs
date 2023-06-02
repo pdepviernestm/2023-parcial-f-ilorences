6861b8844d77f5f8724911e2a95fc83bfe8784c3
ValentinaFabris <vfabris@frba.utn.edu.ar>
2023-06-02 09:58:12 -0300

    ejemplosPunto1

--- main.hs
+++ main.hs
@@ -20,15 +20,21 @@ alterarFelicidad :: Int -> Actividad
 alterarFelicidad num pers = pers {felicidad = max (felicidad pers + num) 0}
 
 comerDonas :: Int -> Actividad
-comerDonas cant = alterarDinero 10 . alterarFelicidad (10 * cant)
+comerDonas cant = alterarDinero (-10) . alterarFelicidad (10 * cant)
 
 alterarDinero :: Int -> Actividad
 alterarDinero num pers = pers {dinero = dinero pers + num}
 
 irTrabajo :: String -> Actividad
-irTrabajo trabajo
-  | trabajo == "escuela elemental" = alterarFelicidad 20 . alterarDinero (cuantoDinero trabajo)
-  | otherwise = alterarDinero (cuantoDinero trabajo)
+irTrabajo trabajo = alterarDinero (cuantoDinero trabajo)
+
+--irTrabajo' :: String -> Actividad
+--irTrabajo' trabajo
+
+-- | trabajo == "escuela elemental" = alterarFelicidad (-20= . alterarDinero (cuantoDinero trabajo)
+-- | otherwise = alterarDinero (cuantoDinero trabajo)
+trabajarDirector :: Actividad
+trabajarDirector = alterarFelicidad (-20) . alterarDinero (cuantoDinero "escuela elemental")
 
 cuantoDinero :: [a] -> Int
 cuantoDinero = length
@@ -44,4 +50,14 @@ homero = UnPersonaje "homero" 100 100
 
 skinner = UnPersonaje "skinner" 50 15
 
-lisa = UnPersonaje "lisa" 20 50
+lisa = UnPersonaje "lisa" 80 50
+
+-- * Main> comerDonas 12 homero
+-- UnPersonaje {nombre = "homero", dinero = 90, felicidad = 220}
+
+-- * Main> trabajarDirector skinner
+--  UnPersonaje {nombre = "skinner", dinero = 67, felicidad = 0}
+
+-- *Main> tomarMedicamento (irEscuela lisa)
+-- UnPersonaje {nombre = "lisa", dinero = 30, felicidad = 50}
+
