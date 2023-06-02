2e58647b4cd798190abd321963f27d4225134bd3
ValentinaFabris <vfabris@frba.utn.edu.ar>
2023-06-02 09:45:41 -0300

    punto1

--- .github/main.hs
+++ .github/main.hs
@@ -7,14 +7,41 @@ data Personaje = UnPersonaje
   }
   deriving (Show, Eq)
 
+--Punto 1
+
 type Actividad = Personaje -> Personaje
 
 irEscuela :: Actividad
 irEscuela pers
-  |nombre pers == "lisa" = alterarFelicidad 20 pers
-  |otherwise = alterarFelicidad (-20) pers
+  | nombre pers == "lisa" = alterarFelicidad 20 pers
+  | otherwise = alterarFelicidad (-20) pers
 
-alterarFelicidad :: Int -> Personaje -> Personaje
-alterarFelicidad num pers = pers{felicidad = felicidad pers + num}
+alterarFelicidad :: Int -> Actividad
+alterarFelicidad num pers = pers {felicidad = max (felicidad pers + num) 0}
 
+comerDonas :: Int -> Actividad
+comerDonas cant = alterarDinero 10 . alterarFelicidad (10 * cant)
 
+alterarDinero :: Int -> Actividad
+alterarDinero num pers = pers {dinero = dinero pers + num}
+
+irTrabajo :: String -> Actividad
+irTrabajo trabajo
+  | trabajo == "escuela elemental" = alterarFelicidad 20 . alterarDinero (cuantoDinero trabajo)
+  | otherwise = alterarDinero (cuantoDinero trabajo)
+
+cuantoDinero :: [a] -> Int
+cuantoDinero = length
+
+irAEscuela :: Actividad
+irAEscuela = alterarFelicidad (-20)
+
+--Inventado
+tomarMedicamento :: Actividad
+tomarMedicamento = alterarFelicidad (-20) . alterarDinero (-50)
+
+homero = UnPersonaje "homero" 100 100
+
+skinner = UnPersonaje "skinner" 50 15
+
+lisa = UnPersonaje "lisa" 20 50
